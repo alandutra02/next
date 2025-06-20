487b5ff48e07638d4fe226ed901269cb696443eb
alandutra02 <alandutra02@yahoo.com.br>
2025-06-20 17:49:27 -0300

    Navegacao

--- fundamentos/src/components/Navegador.jsx
+++ fundamentos/src/components/Navegador.jsx
@@ -1,6 +1,7 @@
 import styles from '../styles/Navegador.module.css'
 import Link from "next/link"
 export default function Navegador(props) {
+    // styles presente na propriedade da div abaixo é uma forma de inserir estilo inline no jsx e isso é feito em forma de objeto. O primeiro par de chaves representa o espaço para comandos javascript enquanto o par de chave mais interno é o objeto que vai conter o css inline do jsx.
     return (
         <div>
             <Link href={props.destino} style={{
--- fundamentos/src/pages/index.jsx
+++ fundamentos/src/pages/index.jsx
@@ -1,6 +1,6 @@
-import Link from "next/link"
 import Navegador from "@/components/Navegador"
 export default function Inicio() {
+    // styles presente na propriedade da div abaixo é uma forma de inserir estilo inline no jsx e isso é feito em forma de objeto. O primeiro par de chaves representa o espaço para comandos javascript enquanto o par de chave mais interno é o objeto que vai conter o css inline do jsx.
     return (
         <div style={{
             display: 'flex',
@@ -12,6 +12,7 @@ export default function Inicio() {
             <Navegador destino='/estiloso' menu="Estiloso"/>
             <Navegador destino='/exemplo' menu="Exemplo" cor="#9400d3" />
             <Navegador destino='/jsx' menu="JSX" cor="crimson" />
+            <Navegador destino='/navegacao' menu="Navegação #01" cor="green" />
         </div>
     )
 }
