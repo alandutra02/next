5f54e7a4905b8062afde82a6a34d0af1c565884f
alandutra02 <alandutra02@yahoo.com.br>
2025-06-20 02:24:57 -0300

    Layout

--- fundamentos/src/pages/estiloso.jsx
+++ fundamentos/src/pages/estiloso.jsx
@@ -2,9 +2,9 @@ import styles from '../styles/Estiloso.module.css'
 import Layout from '@/components/Layout'
 export default function Estiloso() {
     return (
-        <Layout titulo="Exemplo de CSS Modularizado">
-            <div className={styles.roxo}> 
-                <h1>Estilo usando CSS módulos</h1>
+        <Layout titulo="Exemplo de CSS Modularizado"> {/* props.titulo foi definido também dentro do arquivo Layout em {props.titulo}. */}
+            <div className={styles.roxo}> {/* Todo o conteúdo dentro desta div, assim como o h1, são considerados children do módulo Layout. O módulo Layout é importado aqui e haverá a renderização do {props.children} que está presente dentro do arquivo Layout.jsx*/}
+                <h1>Estilo usando CSS módulos</h1> 
             </div>
         </Layout>
     )
